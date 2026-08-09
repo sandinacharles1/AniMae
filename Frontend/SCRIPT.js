25fa80ac3d4fe93a08a2d22fa85f9591471ce338
Sandina Charles <sandinacharles1@gmail.com>
2026-08-08 21:36:40 -0400

    Make search button expand on hover and start basics of JavaScript interactions.

---
+++ Frontend/SCRIPT.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('search-button'); //From html document where we assigened an id, get the id and assign to a variable
+button.addEventListener("click", function() { //add an event listener to the button, when clicked, it will run the function
+
+})
+button.addEventListener("mouseover", function() { //add an event listener to the button, when mouse is over it, it will run the function
+    button.style.; //make it slightly bigger
+}
