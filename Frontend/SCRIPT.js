e52de993e62a6df03e66a340262df87c3c0d63b2
Sandina Charles <sandinacharles1@gmail.com>
2026-08-08 23:08:00 -0400

    Have search bar open a search  menu, dropdown for language, checkbox for filters,set in the middle of the screen.

--- Frontend/SCRIPT.js
+++ Frontend/SCRIPT.js
@@ -1,7 +1,8 @@
-const button = document.getElementById('search-button'); //From html document where we assigened an id, get the id and assign to a variable
-button.addEventListener("click", function() { //add an event listener to the button, when clicked, it will run the function
+//IF we select an item with querySelector, you can do by id (#), class (.), or jhtml element (h1, p, etc). If you select by id, it will only select the first one. It will only choose the first though. querySelectorAll() returns all matching
+// array.forEach(array =>{func ;})
 
+const button = document.getElementById('search-button'); //From html document where we assigened an id, get the id and assign to a variable
+const searchArea = document.querySelector("#search-area");
+button.addEventListener("click", () => { //add an event listener to the button, when clicked, it will run the function
+    searchArea.classList.remove("hidden");
 })
-button.addEventListener("mouseover", function() { //add an event listener to the button, when mouse is over it, it will run the function
-    button.style.; //make it slightly bigger
-}
