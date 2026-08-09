8773448b792ff9ef7fc973c161a5cb25670759b2
Sandina Charles <sandinacharles1@gmail.com>
2026-08-09 14:55:40 -0400

    First attempt at connecting to Jikan API

--- Frontend/SCRIPT.js
+++ Frontend/SCRIPT.js
@@ -1,8 +1,36 @@
 //IF we select an item with querySelector, you can do by id (#), class (.), or jhtml element (h1, p, etc). If you select by id, it will only select the first one. It will only choose the first though. querySelectorAll() returns all matching
 // array.forEach(array =>{func ;})
 
+// OPENING SEARCH AREA
 const button = document.getElementById('search-button'); //From html document where we assigened an id, get the id and assign to a variable
 const searchArea = document.querySelector("#search-area");
 button.addEventListener("click", () => { //add an event listener to the button, when clicked, it will run the function
     searchArea.classList.remove("hidden");
 })
+
+//SEARCH INPUT 
+const submit = document.getElementById('submit-search')
+async function getUserInput(){
+    const text = document.getElementById('search-bar');
+    const language = document.getElementById('Language');
+    const rating = document.getElementById('18');
+
+    const textInput = text.value;
+    const languageInput = language.value;
+    const ratingInput = rating.checked;  //All use values unles sis true or false
+
+    console.log(textInput);
+    console.log(languageInput);
+    console.log(ratingInput);
+
+     //API REQUESTS FOR INFORMATION
+    const query = `?q=${encodeURIComponent(textInput)}`;  //Uses backtick cause it mixes string and variable.  Turns into url
+    const response = await fetch('https://api.jikan.moe/v4/anime/${query}/episodes');
+    const data = await response.json();
+    console.log(data)
+}
+submit.addEventListener("click", () => {
+    getUserInput();
+})
+
+
